3a30ca9bcd9f1fec61b9f889e83c1c8afe2bbd6c
Sabrinafogliene <sabrina.castilho4@gmail.com>
2024-04-11 21:14:28 -0300

    Arquivos de exercícios já feitos

---
+++ Tipos de dados.py
@@ -0,0 +1,7 @@
+print(11+10)
+print(1.5 + 2.9)
+print("Phyton")
+print(True)
+print (False)
+
+int( )
---
+++ Variaveis_Constantes.py
@@ -0,0 +1,10 @@
+nome = "Sabrina"
+idade = 31
+
+print(nome, idade)
+
+nome = "Giovana"
+
+print(nome, idade)
+
+limite_saque_diario = 1000
