71544fe25dd360b5f74a8a8672feef4013c18c08
Sabrinafogliene <sabrina.castilho4@gmail.com>
2024-04-12 22:59:49 -0300

    Exercícios de estruturas condicionais

---
+++ estrutura_condicional_aninhada.py
@@ -0,0 +1,20 @@
+conta_normal = True
+conta_universitaria = False
+saldo = 2500
+saque = 2000
+cheque_especial = 450
+
+if conta_normal:
+    if saldo >= saque:
+        print("Saque realizado com sucesso!")
+    elif saque <= (saldo + cheque_especial):
+        print("Saque realizado com o uso do cheque especial!")
+    else:
+        print ("Não foi possível realizar o saque, saldo insuficiente!")
+elif conta_universitaria:
+    if saldo >= saque:
+        print ("Saque realizado com sucesso!")
+    else:
+        print ("Saldo insuficiente!")
+else: 
+    print("Sistema não reconheceu seu tipo de conta, entre em contato com seu gerente!")
