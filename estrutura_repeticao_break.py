6360aa921adb9707ce4c871726090395c441ffa1
Sabrinafogliene <sabrina.castilho4@gmail.com>
2024-04-12 23:51:39 -0300

    Exercícios de estruturas de repetição

---
+++ estrutura_repeticao_break.py
@@ -0,0 +1,24 @@
+
+while True:
+    numero = int(input ("Informe um número: "))
+
+    if numero == 10:
+        break
+
+    print(numero, end=" ")
+    print()
+
+
+for numero in range (100):
+    if numero == 10:
+        continue
+
+    print(numero, end=" ")
+
+
+
+for numero in range (100):
+    if numero % 2 == 0:
+        continue
+
+    print(numero, end=" ")
