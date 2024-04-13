4a1f4a0b58d2ff07a19da40a692c602128ba1904
Sabrinafogliene <sabrina.castilho4@gmail.com>
2024-04-12 22:09:22 -0300

    exercício operadores de associação

---
+++ operadores_associacao.py
@@ -0,0 +1,17 @@
+frutas = ["Limão", "uva"]
+print ("Laranja" in frutas)
+
+frutas = ["Limão", "uva", "Laranja"]
+print ("Laranja" in frutas)
+
+#case sensitive = qualquer letra diferente dará resultados diferentes.
+
+frutas = ["Limão", "uva", "Laranja"]
+print ("laranja" in frutas)
+
+print("laranja" not in frutas)
+print("Limão" in frutas)
+
+curso = "Curso de Phyton"
+print ("Phyton" in curso)
+print ("phyton" in curso)
